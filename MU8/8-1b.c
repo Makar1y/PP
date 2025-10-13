c24d0f071d9bb3e3f91c01f9a7e36cf1b9a7ded4
Makar1y <makariyteam@gmail.com>
2025-10-13 22:32:47 +0300

    still still Copilot quota reached)

---
+++ MU8/8-1b.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TEST_ARRAY_SIZE 10
+
+void fillArray(int *array, const int size, const int low, const int high) {
+    srand(time(NULL));
+
+    for (int i = 0; i < size; ++i) {
+        *(array + i) = (rand() % (high - low + 1) ) + low;
+    }
+}
+
+
+int * createArray(const int size, const int low, const int high) {
+    int *arr = calloc(size, sizeof(int)); 
+
+    if ((arr != NULL) && (low <= high)) {
+        fillArray(arr, size, low, high);
+        return arr;
+    }
+    return NULL;
+}
+
+
+int main() {
+    
+    int *test = createArray(TEST_ARRAY_SIZE, 0, 100);
+    if (test != NULL) {
+        for (int i = 0; i < TEST_ARRAY_SIZE; ++i) {
+            printf("%d ", test[i]);
+        }
+    }
+    free(test);
+    return 0;
+}
---
+++ MU8/8-2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+long getFileSize(const char *fileName) {
+    long size;
+    FILE *target_file;
+
+    target_file = fopen(fileName, "rb");
+
+    if (target_file != NULL) {
+
+        fseek(target_file, 0, SEEK_END);
+
+        size = ftell(target_file);
+
+        return size;
+    }
+
+    return -1;
+}
+
+
+char* largest_input_file(const int argc, char *argv[]){
+    char *largest = NULL;
+    long max_size = -1;
+    long tmp_size = 0;
+
+    for (int i = 1; i < argc; ++i) {
+        tmp_size = getFileSize(argv[i]);
+
+        if ((tmp_size >= 0) && (tmp_size > max_size)) {
+            largest = argv[i];
+            max_size = tmp_size;
+        }
+    }
+
+    return largest;
+}
+
+
+int main(const int argc, char *argv[]) {
+    const char *name = largest_input_file(argc, argv);
+    if (name != NULL) 
+        printf("%s\n", name);
+    else
+        printf("Error or no files get.");
+    return 0;
+}
