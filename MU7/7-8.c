d9842493ef7b1775bdc3a1e1e5282071ac63c1d2
Makar1y <makariyteam@gmail.com>
2025-10-16 22:28:19 +0300

    Refactor quicksort implementation to use non-recursive approach and improve median selection

--- MU7/7-8.c
+++ MU7/7-8.c
@@ -12,8 +12,23 @@ void swap(int *a, int *b) {
 }
 
 
+int medianThree(int *array, int a, int b, int c) {
+    int a_a = array[a];
+    int a_b = array[b];
+    int a_c = array[c];
+
+    if ((a_a > a_b) ^ (a_a > a_c)) 
+        return a;
+    else if ((a_b < a_a) ^ (a_b < a_c)) 
+        return b;
+    else
+        return c;
+}
+
 int sort(int array[], const int start, const int end) {
     // Base element
+    int base_index = medianThree(array, start, start + (end - start) / 2, end);
+    swap(&array[base_index], &array[end]);
     int base = array[end];
 
     // Starting point
@@ -48,17 +63,36 @@ void rec_quicksort(int array[], const int start, const int end) {
     }
 }
 
-void nrec_quicksort(int array[], const int start, const int end) {
-    // If more than 1 element
-    if (start < end) {
+void nrec_quicksort(int array[], int start, int end) {
+    int mem[end - start + 1];
+    int current = -1;
+
+    mem[++current] = start;
+    mem[++current] = end;
+
+    while (current >= 0) {
+
+        end = mem[current--];
+        start = mem[current--];
 
         // Sort + return new base
         int base = sort(array, start, end);
+
+        if (base > start) {
+            mem[++current] = start;
+            mem[++current] = base - 1;
+        }
+        if (base < end) {
+            mem[++current] = base + 1;
+            mem[++current] = end; 
+        }
 
         // Recursion for left and right side of base element
         // rec_quicksort(array, base + 1, end);
         // rec_quicksort(array, start, base - 1);
     }
+
+
 }
 
 
@@ -71,7 +105,7 @@ int main() {
         array[i] = rand() % 101;
     }
 
-    rec_quicksort(array, 0, ARRAY_SIZE - 1);
+    nrec_quicksort(array, 0, ARRAY_SIZE - 1);
 
     for (int i = 0; i < ARRAY_SIZE; ++i) {
         printf(" %d ", array[i]);
