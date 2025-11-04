0ec4521df2e1704b78307a2907f635a9d720ed18
Makar1y <112717875+Makar1y@users.noreply.github.com>
2025-11-04 19:28:05 +0200

    Refactor file handling: update file operations in main function, remove redundant code, and enhance array filling logic

--- MU11/1/11-1.c
+++ MU11/1/11-1.c
@@ -12,8 +12,6 @@
 
 void fillArray(int *array, size_t size, const int low, const int high) {
    if (array) {
-      srand(time(NULL));
-
       for (int i = 0; i < size; ++i) {
          *(array + i) = (rand() % (high - low + 1) ) + low;
       }
@@ -32,18 +30,19 @@ void printArray(int *array, size_t size) {
 int main(void) {
    int m1[SIZE];
    int m2[SIZE];
-   FILE *test = fopen(FILENAME, "wb");
+   FILE *file = fopen(FILENAME, "wb");
 
-   assert(m1 && m2 && test);
+   assert(m1 && m2 && file);
+   srand(time(NULL));
 
    fillArray(m1, SIZE, 0, 10);
    printArray(m1, SIZE);
-   saveToFile(test, m1, SIZE);
+   saveToFile(file, m1, SIZE);
 
-   test = freopen(FILENAME, "rb", test);
-   assert(test);
+   file = freopen(FILENAME, "rb", file);
+   assert(file);
 
-   loadFromFile(test, m2);
+   loadFromFile(file, m2);
    printArray(m2, SIZE);
 
    for (int i = 0; i < SIZE; ++i) {
