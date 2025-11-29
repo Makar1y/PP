36aa951f9547a95f1f7908a77f80d4e3c088cb41
Makar1y <112717875+Makar1y@users.noreply.github.com>
2025-11-29 22:12:49 +0200

    add burble sorting alg..

--- mu12/1/algorithms.c
+++ mu12/1/algorithms.c
@@ -2,6 +2,18 @@
 
 void burble(int *array, size_t arr_size) {
    if (array) {
-      
+      int swapped, tmp;
+
+      do {
+         swapped = 0;
+         for (int i = 0; i < arr_size-1; ++i) {
+            if (array[i] > array[i + 1]) {
+               int tmp = array[i];
+               array[i] = array[i + 1];
+               array[i + 1] = tmp;
+               swapped = 1;
+            }
+         }
+      } while (swapped);
    }
 }
